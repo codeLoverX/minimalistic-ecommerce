d68fe0766e055a2e92154b1ab8517aa6ac31b1d9
Ridwan Monjur <mjrrdn@gmail.com>
2023-02-13 08:24:16 +0800

    feat(deploy): add production url

--- junior-react-frontend/src/services/gqlClient.js
+++ junior-react-frontend/src/services/gqlClient.js
@@ -1,6 +1,6 @@
 import { GraphQLClient } from "graphql-request";
 
-export const API_URL = 'http://localhost:4000'
+export const API_URL = 'https://scandi-react-ecommerce-git-main-codeloverx.vercel.app'
 
 export const graphQLClient = new GraphQLClient(API_URL)
 
