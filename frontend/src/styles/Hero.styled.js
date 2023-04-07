55e4443d1c9d7322b675da4607d48015d7e45740
Ridwan Monjur <mjrrdn@gmail.com>
2023-04-07 21:52:12 +0800

    change padding 15 pixel

--- frontend/src/styles/Hero.styled.js
+++ frontend/src/styles/Hero.styled.js
@@ -16,7 +16,7 @@ const HeroStyled = styled.div`
 `
 HeroStyled.Text = styled.div`
     background: ${({ theme }) => `${theme.colors.hoverBg}`};
-    padding-top: 20px;
+    padding-top: 15px;
     padding-right: 5%;
     padding-left: 5%;
     button{
